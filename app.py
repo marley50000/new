355298fc145e6b7b62a31637153aedfeab242ccb
marley50000 <152743191+marley50000@users.noreply.github.com>
2025-06-20 17:22:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='static')
-
-# Serve the main page
-@app.route('/')
-def index():
-    return send_from_directory('static', 'auth.html')
-
-# Serve other HTML pages if needed
-@app.route('/<path:filename>')
-def serve_file(filename):
-    return send_from_directory('static', filename)
-
-if __name__ == '__main__':
-    app.run(debug=True)
